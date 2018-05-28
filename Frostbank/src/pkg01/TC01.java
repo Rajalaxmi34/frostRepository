4c07c3c1412f65d0f17be7ad88d3b9b872c21b9c
Rajalaxmi34 <rjmohanty.94@gmail.com>
2018-05-28 09:49:30 +0530

    created automation project

---
+++ Frostbank/src/pkg01/TC01.java
@@ -0,0 +1,47 @@
+package pkg01;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TC01 {
+	
+	WebDriver driver;
+	
+	@BeforeClass
+	
+	public void lunchBrowser(){
+		
+		driver = new FirefoxDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.frostbank.com/");
+	}
+	
+	@Test
+	
+	public void testcase(){
+		
+		driver.findElement(By.id("username-field")).sendKeys("abc");
+		
+		driver.findElement(By.id("password-field")).sendKeys("abc");
+		
+		driver.findElement(By.id("login-button")).click();
+		
+		
+		
+	}
+	
+	@AfterClass
+	
+	public void quit(){
+		
+		//driver.quit();
+		
+	}
+
+}
